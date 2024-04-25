cc7115a0fd6724c2c1fadec86e0976d4c77cec2a
chacha <chacha@sparcs.org>
2024-04-25 11:43:07 +0900

    Prob. 1 to 3 done (chacha)

--- checker.ts
+++ checker.ts
@@ -9,19 +9,91 @@ const prisma = new PrismaClient()
 
 
 function problem1() {
-  return prisma.$queryRaw`select firstName, lastName, income from Customer where income <= 60000 and income >= 50000 order by income desc, lastName asc, firstName asc LIMIT 10;`
+  return prisma.$queryRaw`
+  select 
+    firstName, lastName, income 
+  from Customer 
+  where 
+    income <= 60000 and income >= 50000 
+  order by 
+    income desc, lastName asc, firstName asc 
+  LIMIT 10;`
 }
 
 function problem2() {
-  return prisma.$queryRaw`select * from Customer`
+  return prisma.$queryRaw`
+  SELECT
+    e.sin AS "sin",
+    (SELECT branchName
+      FROM Branch
+      WHERE Branch.branchNumber = e.branchNumber) AS "branchName",
+    e.salary AS "salary",
+    CAST((
+      SELECT man.salary
+      FROM Employee AS man
+      WHERE man.sin = (
+        SELECT managerSIN
+        FROM Branch
+        WHERE e.branchNumber = Branch.branchNumber
+      )
+    ) - e.salary AS CHAR(5)) AS "Salary Diff"
+  FROM
+      Employee AS e
+  WHERE
+      (SELECT branchName
+      FROM Branch
+      WHERE Branch.branchNumber = e.branchNumber) = 'London'
+      OR
+      (SELECT branchName
+      FROM Branch
+      WHERE Branch.branchNumber = e.branchNumber) = 'Berlin'
+  ORDER BY
+      (
+        SELECT man.salary
+        FROM Employee AS man
+        WHERE man.sin = (
+          SELECT managerSIN
+          FROM Branch
+          WHERE e.branchNumber = Branch.branchNumber
+        )
+      ) - e.salary DESC
+  LIMIT 10;
+  `
 }
 
 function problem3() {
-  return prisma.$queryRaw`select * from Customer`
+  return prisma.$queryRaw`
+  SELECT 
+    c.firstName, c.lastName, c.income 
+  FROM Customer AS c
+  WHERE income >= ALL (
+    SELECT 2 * income
+    FROM Customer
+    WHERE lastName = "Butler"
+  )
+  ORDER BY lastName ASC, firstName ASC
+  LIMIT 10;`
 }
 
 function problem4() {
-  return prisma.$queryRaw`select * from Customer`
+  return prisma.$queryRaw`
+  SELECT 
+    c.customerID,
+    c.income,
+    o.accNumber,
+    a.branchNumber
+  FROM 
+    Customer as c LEFT OUTER JOIN Owns as o
+    ON c.customerID = o.customerID
+    LEFT OUTER JOIN Account AS a
+    ON o.accNumber = a.accNumber
+  WHERE 
+    (c.income > 80000) 
+    AND
+    (a.branchNumber IN (1, 2))
+  ORDER BY c.customerID, o.accNumber 
+  LIMIT 10;
+  `
 }
 
 function problem5() {
